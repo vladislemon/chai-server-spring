6925744e3d21dd42a65b620bac295c4aed13aeb8
vladislemon <vladislemon@gmail.com>
2023-07-08 02:20:33 +0400

    Spring initializr

---
+++ src/test/java/net/chai/chaiserverspring/ChaiServerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package net.chai.chaiserverspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaiServerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
