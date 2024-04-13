d379f3bd9f84ef887880483df2fce9c139305966
vladislemon <vladislemon@gmail.com>
2024-04-14 03:16:46 +0600

    Refactor exception handling

--- src/main/java/net/chai/chaiserverspring/common/ControllerExceptionHandlers.java
+++ src/main/java/net/chai/chaiserverspring/common/ControllerExceptionHandlers.java
@@ -1,31 +1,21 @@
 package net.chai.chaiserverspring.common;
 
 import net.chai.chaiserverspring.common.exception.NotFoundException;
-import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.web.ErrorResponseException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class ControllerExceptionHandlers {
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
-    }
-
-    @ExceptionHandler(OptimisticLockingFailureException.class)
-    public ResponseEntity<?> handleOptimisticLockingFailureException() {
-        return ResponseEntity.status(HttpStatus.CONFLICT).build();
-    }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -39,16 +29,13 @@ public class ControllerExceptionHandlers {
         return errors;
     }
 
-    @ExceptionHandler(SQLException.class)
-    public ResponseEntity<?> handleSQLException(SQLException e) {
-        if (e.getErrorCode() == 0) { // PSQL unique constraint violation
-            return ResponseEntity.status(HttpStatus.CONFLICT).build();
-        }
-        return ResponseEntity.internalServerError().build();
-    }
-
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<?> handleNotFoundException(NotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(ErrorResponseException.class)
+    public ProblemDetail handleErrorResponseException(ErrorResponseException e) {
+        return e.getBody();
     }
 }
--- src/main/java/net/chai/chaiserverspring/common/service/CrudService.java
+++ src/main/java/net/chai/chaiserverspring/common/service/CrudService.java
@@ -7,18 +7,26 @@ import net.chai.chaiserverspring.common.model.CrudCreateRequest;
 import net.chai.chaiserverspring.common.model.CrudDeleteRequest;
 import net.chai.chaiserverspring.common.model.CrudUpdateRequest;
 import net.chai.chaiserverspring.common.model.Entity;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.data.relational.core.conversion.DbActionExecutionException;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.ErrorResponseException;
 
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Slf4j
 @AllArgsConstructor
 public abstract class CrudService<T extends Entity> {
+    private static final Pattern DUPLICATE_KEY_PATTERN = Pattern.compile("Key \\((.*)\\)=\\((.*)\\) already exists");
     private final CrudRepository<T, UUID> repository;
 
     public T create(CrudCreateRequest<T> request) {
         log.info("{}", request);
-        return repository.save(request.toEntity());
+        return save(request.toEntity());
     }
 
     public T get(UUID id) {
@@ -28,11 +36,34 @@ public abstract class CrudService<T extends Entity> {
     public T update(CrudUpdateRequest<T> request) {
         log.info("{}", request);
         T oldEntity = get(request.id());
-        return repository.save(request.toEntity(oldEntity));
+        return save(request.toEntity(oldEntity));
     }
 
     public void delete(CrudDeleteRequest<T> request) {
         log.info("{}", request);
         repository.delete(request.toEntity());
+    }
+
+    protected T save(T entity) {
+        try {
+            return repository.save(entity);
+        } catch (DbActionExecutionException e) {
+            if (e.getCause() instanceof DuplicateKeyException) {
+                Matcher matcher = DUPLICATE_KEY_PATTERN.matcher(e.getCause().getMessage());
+                if (matcher.find()) {
+                    throw conflict(String.format("%s with %s '%s' already exists", entity.getClass().getSimpleName(), matcher.group(1), matcher.group(2)));
+                }
+                throw conflict(String.format("Similar %s already exists", entity.getClass().getSimpleName()));
+            }
+            throw e;
+        } catch (OptimisticLockingFailureException e) {
+            throw conflict(String.format("%s has been modified from another source", entity.getClass().getSimpleName()));
+        }
+    }
+
+    protected static ErrorResponseException conflict(String detail) {
+        ErrorResponseException exception = new ErrorResponseException(HttpStatus.CONFLICT);
+        exception.setDetail(detail);
+        return exception;
     }
 }
